c76b82a85e81a15b758ab5ba6e59eafce4b9c339
Dnyaneshwar Rathod <151998044+DnyanuDeveloper7498@users.noreply.github.com>
2026-08-03 23:38:12 +0530

    Time: 1 ms (97.44%), Space: 47.3 MB (60.71%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -10,12 +10,13 @@ class Solution {
 
      int left=0;
 
-     for(int i=0;i<nums.length; left+=nums[i++])
+     for(int i=0;i<nums.length; i++)
      {
-        if((left*2)==totalSum-nums[i])
+        if(left==totalSum-left-nums[i])
         {
             return i;
         }
+        left+=nums[i];
      }
       return -1;
     }

e71cee6781edeedb6bc25e0f4bd3dd242169b235
Dnyaneshwar Rathod <151998044+DnyanuDeveloper7498@users.noreply.github.com>
2026-08-04 09:55:26 +0530

    Time: 3 ms (100%), Space: 175.8 MB (8.46%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,24 +1,34 @@
 class Solution {
-    public int[] rearrangeArray(int[] nums) {
-        
-      int ans[]=new int[nums.length];
+  
+  static {
+      Solution sol=new Solution();
 
-      int pos=0;
-      int neg=1;
-
-      for(int i=0;i<nums.length;i++)
+      for(int i=0;i<500;i++)
       {
-        if(nums[i]>0)
-        {
+        sol. rearrangeArray(new int []{1,-1});
+      }
+  }
+
+
+   public int[] rearrangeArray(int[] nums) {
+
+        int ans[]=new int [nums.length];
+        int pos=0;
+        int neg=1;
+
+       for(int i=0;i<nums.length;i++)
+       {
+          if(nums[i]>0)
+          {
             ans[pos]=nums[i];
             pos+=2;
-        }
-        else
-        {
+          }
+          else
+          {
             ans[neg]=nums[i];
             neg+=2;
-        }
-      }
-      return ans;
+          }
+       }
+       return ans; 
     }
 }

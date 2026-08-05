942fdfaf8ca826b1894d067367c7fc394b27ae03
Dnyaneshwar Rathod <151998044+DnyanuDeveloper7498@users.noreply.github.com>
2026-08-05 10:26:23 +0530

    Time: 1 ms (98.5%), Space: 42.5 MB (70.58%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,18 +1,45 @@
 class Solution {
 
 
+     static {
+         Solution sol=new Solution();
 
-
+         for(int i=0;i<500;i++)
+         {
+            sol.digitFrequencyScore(0);
+         }
+     }
     public int digitFrequencyScore(int n) {
 
-    int sum=0;
+       int sum=0;
+       for(int i=0;i<=9;i++)
+       {
+         int ct=0;
+         int temp=n;
+         int mul=0;
 
-      while(n!=0)
-      {
-        sum+=n%10;
-        n/=10;
-      }
+         while(temp!=0)
+         {
+            if(temp%10==i)
+            {
+                ct++;
+            }
+            temp/=10;
+         }
+        
+         if(ct>0)
+         {
+            mul=ct*i;
+
+            sum+=mul;
+         }
+ 
+       }
+      
       return sum;
      
-    }   
+    }
+
+        
+    
 }

cd2ef8368683ac779956ec1bd22dd32cac5625d2
Dnyaneshwar Rathod <151998044+DnyanuDeveloper7498@users.noreply.github.com>
2026-08-06 08:29:32 +0530

    Time: 1 ms (99.83%), Space: 77.5 MB (29.77%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,25 +1,45 @@
 class Solution {
+
+  static 
+  {
+    Solution sol=new Solution();
+
+    for(int i=0;i<500;i++)
+    {
+        sol.maxArea(new int []{0});
+    }
+  }
+
     public int maxArea(int[] height) {
+
+        int left=0;
+        int right=height.length-1;
+        int maxArea=0;
+
+      while(left<right)
+      {
+          int area;
+
+          if(height[left]<height[right])
+          {
+            area=height[left]*(right-left);
+
+            if(area>maxArea)
+                maxArea=area;
+                left++;
+          }
+          else
+          {
+             area=height[right]*(right-left);
+               if(area>maxArea)
+                maxArea=area;
+                right--;
+          }
+         
+      }
+    
+
+       return maxArea;
         
-       int left=0;
-       int right=height.length-1;
-       int maxArea=0;
-       while (left<=right)
-       { 
-           int area=Math.min(height[left],height[right])*(right-left);
-           maxArea=Math.max(maxArea,area);
-           
-           if(height[left]<height[right])
-           {
-            left++;
-           }
-           else
-           {
-            right--;
-           }
-       }
-
-  return maxArea;
-
     }
 }

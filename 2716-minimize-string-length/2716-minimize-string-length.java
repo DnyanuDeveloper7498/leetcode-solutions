0355af83a5a61e8eef2ef48c43d8b41d94115918
Dnyaneshwar Rathod <151998044+DnyanuDeveloper7498@users.noreply.github.com>
2026-08-22 01:03:09 +0530

    Time: 3 ms (99.92%), Space: 46.2 MB (96.83%) - LeetHub

---
+++ 2716-minimize-string-length/2716-minimize-string-length.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+
+
+        int freq[]=new int [26];
+      
+         for(int i=0;i<s.length();i++)
+         {
+            freq[s.charAt(i)-'a']++;
+         }
+
+         int ct=0;
+
+         for(int i=0;i<freq.length;i++)
+         {
+            if(freq[i]!=0)
+            ct++;
+         }
+        return ct;
+    }
+}

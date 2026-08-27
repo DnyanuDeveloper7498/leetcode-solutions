93115225f82d8ed76f1e3c3ce114d7ff24f63056
Dnyaneshwar Rathod <151998044+DnyanuDeveloper7498@users.noreply.github.com>
2026-08-27 10:10:51 +0530

    Time: 1 ms (29.51%), Space: 47.2 MB (66.15%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,35 +1,33 @@
 class Solution {
-
     public int missingNumber(int[] nums) {
-   
-    
+
       int freq[]=new int [max(nums)+2];
 
       for(int num:nums)
       {
-        freq[num]++;
-      }
+          freq[num]++;
+      }    
 
       for(int i=0;i<freq.length;i++)
       {
         if(freq[i]==0)
-         return i;
+        {
+            return i;
+        }
       }
-       return -1;
+    return -1;
 
     }
-
-
 
     public static int max(int arr[])
     {
         int max=Integer.MIN_VALUE;
 
-       for(int num:arr)
-       {
-          if(num>max)
-           max=num;
-       }
-       return max;
+        for(int num:arr)
+        {
+            if(num>max)
+            max=num;
+        }
+        return max;
     }
 }

e8620a94a5c1991178c4d44bf9f1dbf3e135d852
Dnyaneshwar Rathod <151998044+DnyanuDeveloper7498@users.noreply.github.com>
2026-08-31 11:47:57 +0530

    Time: 0 ms (100%), Space: 42.9 MB (14.57%) - LeetHub

--- 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -5,27 +5,14 @@ class Solution {
 
         int ct=0;
 
-        int ct2=0;
-
         for(int i=0;i<s.length();i++)
         {
-            if(s.charAt(i)==' ')
-            { 
-                 if(i>0 && s.charAt(i-1)!=' ')
-                 {
-                    ct++;
-                 }
-            }else
-             {
-                    ct2++;
-             }
+            if(s.charAt(i)!=' ' && (i==0|| s.charAt(i-1)==' '))  
+            {
+            ct++;
+            }
         }
 
-
-        if(ct2==0) return 0;
-
-        if(s.charAt(s.length()-1)!=' ')
-         return ct+1;
 
          return ct;
     }

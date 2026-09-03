93f0eb542ff5bce2306771d43826c72c0eee33e0
Dnyaneshwar Rathod <151998044+DnyanuDeveloper7498@users.noreply.github.com>
2026-09-03 22:46:34 +0530

    Time: 6 ms (44.02%), Space: 44.5 MB (59.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,39 +1,24 @@
 class Solution {
-
-  static {
-      Solution sol=new Solution();
-
-      for(int i=0;i<500;i++)
-      {
-        sol.isAnagram("hellow","hellow");
-      }
-  }
-
     public boolean isAnagram(String s, String t) {
-       int freqS[]=new int [26];
-
-       int freqT[]=new int [26];
-
-       for(int i=0;i<s.length();i++)
-       {
-          freqS[s.charAt(i)-'a']++;
-       }
-       
-        for(int i=0;i<t.length();i++)
-       {
-          freqT[t.charAt(i)-'a']++;
-       }
-       
 
 
-      for(int i=0;i<freqS.length;i++)
+      if(s.length()!=t.length()) return false;
+      
+      int freq[]=new int [26];
+
+      for(int i=0;i<s.length();i++)
       {
-        if(freqS[i]!=freqT[i])
-        {
-            return false;
-        }
+          freq[s.charAt(i)-'a']++;
+          freq[t.charAt(i)-'a']--;
       }
-        return true;
 
+      for(int i=0;i<freq.length;i++)
+      {
+        if(freq[i]!=0)
+        return false;
+      }
+        
+
+    return true;
     }
 }

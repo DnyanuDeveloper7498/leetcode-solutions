7e32b3ed469d5423645c0a47b81097f46f215408
Dnyaneshwar Rathod <151998044+DnyanuDeveloper7498@users.noreply.github.com>
2026-09-05 23:45:47 +0530

    Time: 0 ms (100%), Space: 43.8 MB (79.91%) - LeetHub

--- 3798-largest-even-number/3798-largest-even-number.java
+++ 3798-largest-even-number/3798-largest-even-number.java
@@ -1,15 +1,15 @@
 class Solution {
     public String largestEven(String s) {
 
-      for(int i=s.length()-1;i>=0;i--)
-      {
-         int num=s.charAt(i)-'0';
+        for (int i = s.length() - 1; i >= 0; i--) {
 
-         if(num%2==0)
-         {
-            return s.substring(0,i+1);
-         }
-      }
-      return "";
+            int digit = s.charAt(i) - '0';
+
+            if (digit % 2 == 0) {
+                return s.substring(0, i + 1);
+            }
+        }
+
+        return "";
     }
 }

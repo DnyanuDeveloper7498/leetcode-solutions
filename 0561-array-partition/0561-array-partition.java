ba2d564dbfda49833c4d58acd60209722857bbae
Dnyaneshwar Rathod <151998044+DnyanuDeveloper7498@users.noreply.github.com>
2026-09-14 00:15:00 +0530

    Time: 4 ms (99.93%), Space: 47.7 MB (99.59%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,82 @@
+class Solution {
+
+
+   static 
+    {
+        Solution sol=new Solution();
+
+        for(int i=0;i<500;i++)
+        {
+            sol.arrayPairSum(new int[]{0});
+        }
+    }
+
+
+    public int arrayPairSum(int[] nums) {
+
+    int max=max(nums);
+    int min=min(nums);
+
+
+    int freq[]=new int [max-min+1];
+
+      for(int num:nums)
+      {
+         freq[num-min]++;
+      }
+      
+      int index=0;
+      int sum=0;
+
+      for(int i=0;i<freq.length;i++)
+      {
+         while(freq[i]>0)
+         {
+            int value=i+min;
+
+            if(index%2==0)
+            {
+                sum+=value;
+            }
+
+            index++;
+            freq[i]--;
+         }
+      }
+
+    return sum;        
+    }
+
+
+    public static int max(int arr[])
+    {
+        int max=Integer.MIN_VALUE;
+         
+        for(int num:arr)
+        {
+            if(num>max)
+            {
+                max=num;
+            }
+        }
+
+
+        return max;
+    }
+
+     public static int min(int arr[])
+    {
+        int min=Integer.MAX_VALUE;
+         
+        for(int num:arr)
+        {
+            if(num<min)
+            {
+                min=num;
+            }
+        }
+
+
+        return min;
+    }
+}

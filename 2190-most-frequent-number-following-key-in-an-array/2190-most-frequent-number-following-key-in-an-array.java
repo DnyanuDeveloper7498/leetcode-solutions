6bb4ba50e075ac7f0afb2ce9c3057ef727738ff3
Dnyaneshwar Rathod <151998044+DnyanuDeveloper7498@users.noreply.github.com>
2026-09-26 22:35:55 +0530

    Time: 2 ms (75.1%), Space: 46.5 MB (41.25%) - LeetHub

---
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int mostFrequent(int[] nums, int key) {
+        
+
+        int freq[]=new int [1001];
+
+      for(int i=0;i<nums.length-1;i++)
+      {
+        if(nums[i]==key)
+        {
+            freq[nums[i+1]]++;
+        }
+      }
+
+      int max=Integer.MIN_VALUE;
+
+      int ans=0;
+
+      for(int i=0;i<freq.length;i++)
+      {
+        if(freq[i]>max)
+        {
+            max=freq[i];
+            ans=i;
+        }
+      }
+
+  return ans;
+    }
+}
